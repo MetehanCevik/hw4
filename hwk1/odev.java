1de09fda07075512b23a7b3452f626fe44c07ad5
Metehan <metehaneko@gmail.com>
2024-01-10 20:30:03 +0300

    featured-2 degisiklik

--- hwk1/odev.java
+++ hwk1/odev.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class odev {
     public static void main(String[] args) {
+ 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the first number: ");
